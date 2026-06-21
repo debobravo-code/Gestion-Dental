0592fe396439b9ef962fc9d32b69f643f84c25ce
Debora Bravo <debo.bravo@duocuc.cl>
2026-06-21 15:21:33 -0400

    Agrega gitignore para excluir target

--- reporte-service/target/classes/db/migration/V1__crear_tabla_reportes.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS reportes (
-    id_reporte INT AUTO_INCREMENT PRIMARY KEY,
-    tipo_reporte VARCHAR(100) NOT NULL,
-    fecha_inicio DATE NOT NULL,
-    fecha_fin DATE NOT NULL,
-    fecha_generacion DATE NOT NULL,
-    resumen TEXT,
-    usuario_id INT
-);

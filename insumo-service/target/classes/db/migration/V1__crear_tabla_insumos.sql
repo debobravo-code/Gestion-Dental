8c9da1459b0d2be045c20e863c164c70061a336c
Debora Bravo <debo.bravo@duocuc.cl>
2026-06-23 20:52:44 -0400

    Migracion de application.properties a application.yml y dockerizacion de egreso-service

--- insumo-service/target/classes/db/migration/V1__crear_tabla_insumos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS insumos (
-    id_insumo INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    categoria VARCHAR(100) NOT NULL,
-    unidad_medida VARCHAR(50) NOT NULL,
-    fecha_vencimiento DATE,
-    stock_minimo INT NOT NULL
-);
--- stock-service/target/classes/db/migration/V1__crear_tabla_stock.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS stock (
-    id_stock INT AUTO_INCREMENT PRIMARY KEY,
-    insumo_id INT NOT NULL UNIQUE,
-    cantidad_actual INT NOT NULL DEFAULT 0,
-    cantidad_minima INT NOT NULL DEFAULT 0
-);
